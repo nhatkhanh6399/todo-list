557c07825b927b7cb62c53f534dea91ac8c8cb4d
Nhật Khanh <nhatkhanh@MacBook-Pro.domain.com>
2022-03-21 17:10:57 +0700

    Fix some bugs

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -8,7 +8,7 @@ const TodoForm = ({ onAdd, edit, onEdit }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!input) return;
+    if (!input || /^\s*$/.test(input)) return;
 
     if (!edit) onAdd({ id: uuidv4(), title: input, completed: false });
     else onEdit(edit.id, input);
